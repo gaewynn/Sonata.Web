aff3d0de5c34c40c761fedfdffbdf2a6611e05d4
afi <arnaud.fischer@qiminfo.ch>
2019-07-03 12:13:25 +0200

    Add Extensions methods

---
+++ Sonata.Web/Extensions/HttpContextAccessorExtension.cs
@@ -0,0 +1,53 @@
+﻿#region Namespace Sonata.Web.Extensions
+//	TODO
+#endregion
+
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Threading.Tasks;
+
+namespace Sonata.Web.Extensions
+{
+    public static class HttpContextAccessorExtension
+    {
+        public static string GetBearer(this IHttpContextAccessor instance)
+        {
+            if (instance.HttpContext?.Request == null)
+            {
+                throw new InvalidOperationException($"Either {nameof(instance.HttpContext)} or {instance.HttpContext.Request} is not defined");
+            }
+
+            return instance.HttpContext.Request.GetBearer();
+        }
+
+        public static string GetClaimFromBearerToken(this IHttpContextAccessor instance, string claimsType)
+        {
+            if (instance.HttpContext?.Request == null)
+            {
+                throw new InvalidOperationException($"Either {nameof(instance.HttpContext)} or {instance.HttpContext.Request} is not defined");
+            }
+
+            return instance.HttpContext.Request.GetClaimFromBearerToken(claimsType);
+        }
+
+        public static async Task<string> ReadBodyAsStringAsync(this IHttpContextAccessor instance)
+        {
+            if (instance.HttpContext?.Request == null)
+            {
+                throw new InvalidOperationException($"Either {nameof(instance.HttpContext)} or {instance.HttpContext.Request} is not defined");
+            }
+
+            return await instance.HttpContext.Request.ReadBodyAsStringAsync();
+        }
+
+        public static string GetFirstOrDefaultHeaderValue(this IHttpContextAccessor instance, string headerKey)
+        {
+            if (instance.HttpContext?.Request == null)
+            {
+                throw new InvalidOperationException($"Either {nameof(instance.HttpContext)} or {instance.HttpContext.Request} is not defined");
+            }
+
+            return instance.HttpContext.Request.GetFirstOrDefaultHeaderValue(headerKey);
+        }
+    }
+}
